eab294557db80984713b0e5e0be97a9f418c8859
Iván <imontero22@jaumebalmes.net>
2023-10-03 20:04:48 +0200

    Esta es el tercer commit de la practica

--- ivanMontero_p2.php
+++ ivanMontero_p2.php
@@ -8,60 +8,70 @@
 </head>
 
 <body>
-    
+
+    <?php
+    $minComensal = 0;
+    $maxComensal = 5;
+    $comensales = array();
+    //Primer bucle para crear valor de el número de comensales
+    for ($i = 0; $i < 10; $i++) {
+
+        $randomNumber = rand(0, 5);
+        array_push($comensales, $randomNumber);
+    }
+
+    ?>
+    <ul>
         <?php
-        
-        
-        $minComensal = 0;
-        $maxComensal = 5;
-        $comensales=array();
-     
-
-     
-
-        //Primer bucle para crear valor de el número de comensales
-        for ($i = 0; $i < 10; $i++) 
-        {
-            
-            $randomNumber = rand( 0 , 5 );
-            array_push($comensales,$randomNumber);
-           
-            
-        }
-        
-        ?>
-        <ul>
-            <?php
         //Bucle para mostrar la información
         for ($i = 0; $i < 10; $i++) {
 
             if ($comensales[$i] == 5) //Primera condicion para comensales max
             {
-                ?>
-                    <li>
-                    <?php echo "La taula " . $i."està plena"; ?>
-                    </li>
-                <?php
-            } 
-                else if ($comensales[$i] == 0) //Condicion para comensales con taula buida
+        ?>
+                <li>
+                    <?php echo "La taula " . $i . "està plena"; ?>
+                </li>
+            <?php
+            } else if ($comensales[$i] == 0) //Condicion para comensales con taula buida
             {
-                ?>
-                    <li>
-                   <?php echo "La taula " . $i . "està buida";?>
-                    </li>
-                <?php
-            }
-                else  // Si no es ninguna de las anteriores imprimer el número de comensales restantes.
+            ?>
+                <li>
+                    <?php echo "La taula " . $i . "està buida"; ?>
+                </li>
+            <?php
+            } else  // Si no es ninguna de las anteriores imprimer el número de comensales restantes.
             {
-                ?>
-                    <li>
-                   <?php echo "La taula " . $i . " te " . $comensales[$i] . " comensals";?>
-                    </li>
-                <?php
+            ?>
+                <li>
+                    <?php echo "La taula " . $i . " te " . $comensales[$i] . " comensals"; ?>
+                </li>
+        <?php
             }
         }
-    ?>                   
+        ?>
     </ul>
+    <?php
+    // Ejercicio 2
+    $minPersonas = 0;
+    $maxPersonas = 4;
+    $plantaMin =0;
+    $plantaMax=4;
+    $planta();
+    $habitaciones();
+
+    
+
+    for( $i = 0; $i <5 ; $i++)
+    {
+       for($x =0;$x <10 ; $x++)
+       {
+        $personas = rand($minPersonas,$maxPersonas);
+        array_push($habitaciones,$personas);             
+       }
+      array_push($planta,$habitaciones);
+    }
+    ?>
 </body>
 
 </html>
